68581d283f8e6dac4149f60da3bf036932f35aee
FoolOfShadows <foolofshadows@gmail.com>
2015-01-23 07:18:09 -0600

    Initial Commit

---
+++ Procedures & Injections 2.0/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Procedures & Injections 2.0
+//
+//  Created by Fool on 1/23/15.
+//  Copyright (c) 2015 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
